5094645a010d85392c517bd70398a8050e122df7
Drik Qlis <drikqlis@gmail.com>
2020-01-11 21:59:34 +0100

    1

--- SubSyncStarter.py
+++ SubSyncStarter.py
@@ -3,22 +3,23 @@ import sys
 import logging
 import subprocess
 
-logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', filename='/home/mateusz/subsync/SubSyncStarter.log', level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', level=logging.DEBUG, handlers=[logging.FileHandler("/home/mateusz/subsync/SubSyncStarter.log"),logging.StreamHandler()])
+log = logging.getLogger()
 
 reference_file = sys.argv[1]
 sub_file = sys.argv[2]
 sub_code2 = '.%s.srt' % sys.argv[3]
 sub_code3 = '.%s.srt' % sys.argv[4]
 sub_new = sub_file.replace(sub_code3, sub_code2)
-command = "/snap/bin/subsync --cli --verbose 1 --loglevel 1 --logfile '/home/mateusz/subsync/subsync.log' sync --sub '%s' --ref '%s' --out '%s' --effort 0 --overwrite" % (sub_file, reference_file, sub_new)
+command = "/snap/bin/subsync --cli --verbose 0 --loglevel 0 --logfile '/home/mateusz/subsync/subsync.log' sync --sub '%s' --ref '%s' --out '%s' --effort 0 --overwrite" % (sub_file, reference_file, sub_new)
 
-logging.debug('Reference file: %s' % reference_file)
-logging.debug('Subtitles file: %s' % sub_file)
-logging.debug('Subtitles code (2): %s' % sub_code2)
-logging.debug('Subtitles code (3): %s' % sub_code3)
+log.debug('Reference file: %s' % reference_file)
+log.debug('Subtitles file: %s' % sub_file)
+log.debug('Subtitles code (2): %s' % sub_code2)
+log.debug('Subtitles code (3): %s' % sub_code3)
 
-logging.info('Starting conversion of subtitles file: %s' % sub_file)
-logging.debug('Running command: %s' % command)
+log.info('Starting conversion of subtitles file: %s' % sub_file)
+log.debug('Running command: %s' % command)
 p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
 (output, err) = p.communicate()
 ## Wait for date to terminate. Get return returncode ##
@@ -27,6 +28,6 @@ p_status = p.wait()
 print("Command output : ", output)
 print("Command exit status/return code : ", p_status)
 
-logging.debug('Output: %s' % output)
-logging.debug('Error: %s' % err)
-logging.debug('Exit code: %s' % p_status)
+log.debug('Output: %s' % output)
+log.debug('Error: %s' % err)
+log.debug('Exit code: %s' % p_status)
