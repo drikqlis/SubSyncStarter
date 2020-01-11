8448a52f6cf2bbf8729b8e8671a8b3f6e48a94ed
Drik Qlis <drikqlis@gmail.com>
2020-01-11 19:41:21 +0100

    Fix

--- SubSyncStarter.py
+++ SubSyncStarter.py
@@ -11,6 +11,7 @@ sub_file = sys.argv[2]
 sub_code2 = '.%s.srt' % sys.argv[3]
 sub_code3 = '.%s.srt' % sys.argv[4]
 sub_new = sub_file.replace(sub_code3, sub_code2)
+command = "/snap/bin/subsync --cli --verbose 2 sync --sub %s --ref %s --out %s --effort 1 --loglevel 2 --logfile /var/log/subsync/subsync.log" % sub_file, reference_file, sub_new
 
 logging.debug('Reference file: %s' % reference_file)
 logging.debug('Subtitles file: %s' % sub_file)
@@ -18,7 +19,8 @@ logging.debug('Subtitles code (2): %s' % sub_code2)
 logging.debug('Subtitles code (3): %s' % sub_code3)
 
 logging.info('Starting conversion of subtitles file: %s' % sub_file)
-process = Popen("/snap/bin/subsync --cli --verbose 2 sync --sub %s --ref %s --out %s --effort 1 --loglevel 2 --logfile /var/log/subsync/subsync.log" % sub_file, reference_file, sub_new, stdout=PIPE)
+logging.debug('Running command: %s' % command)
+process = Popen(command, stdout=PIPE)
 (output, err) = process.communicate()
 exit_code = process.wait()
 
