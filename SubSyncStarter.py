2be2bee931f206a2dbec493b8fe08af0625210b4
Drik Qlis <drikqlis@gmail.com>
2020-01-15 21:29:19 +0100

    Added cofiguration file.

--- SubSyncStarter.py
+++ SubSyncStarter.py
@@ -2,17 +2,29 @@ import os
 import sys
 import logging
 import subprocess
-
-logging.root.handlers = []
-logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', level=logging.DEBUG, handlers=[logging.FileHandler("/home/mateusz/subsync/SubSyncStarter.log", encoding="utf-8"),logging.StreamHandler()])
-log = logging.getLogger()
+import configparser
 
 reference_file = sys.argv[1]
 sub_file = sys.argv[2]
 sub_code2 = '.%s.srt' % sys.argv[3]
 sub_code3 = '.%s.srt' % sys.argv[4]
 sub_new = sub_file.replace(sub_code3, sub_code2)
-command = "/snap/bin/subsync --cli --verbose 1 --logfile '/home/mateusz/subsync/subsync.log' sync --sub '%s' --ref '%s' --out '%s' --effort 1 --overwrite" % (sub_file, reference_file, sub_new)
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+loglevel_starter = config['General']['LoggingLevel']
+logfile_starter = config['General']['Logfile']
+location_subsync = config['SubSync']['Location']
+loglevel_subsync = config['SubSync']['LoggingLevel']
+logfile_subsync = config['SubSync']['Logfile']
+effort = config['SubSync']['Effort']
+window_size = config['SubSync']['WindowSize']
+
+logging.root.handlers = []
+logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', level=logging.DEBUG, handlers=[logging.FileHandler(logfile_starter, encoding="utf-8"),logging.StreamHandler()])
+log = logging.getLogger()
+
+command = "/snap/bin/subsync --cli --verbose " + loglevel_subsync + " --logfile '" + logfile_subsync + "' --window-size " + window_size + " sync --sub '" + sub_file + "' --ref '" + reference_file + "' --out '" + sub_new + "' --effort " + effort +" --overwrite"
 
 log.debug('Reference file: %s' % reference_file)
 log.debug('Subtitles file: %s' % sub_file)
